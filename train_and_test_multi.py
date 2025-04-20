1918c03317d4eeb7c17025d8265963461db36c93
Fang yu <129758584+Elica-FY@users.noreply.github.com>
2025-04-20 21:44:10 +0800

    Add files via upload

---
+++ train_and_test_multi.py
@@ -0,0 +1,199 @@
+import Functions
+import torch
+import argparse
+import torch.nn as nn
+import torch.utils.data as Data
+import torch.backends.cudnn as cudnn
+from scipy.io import savemat
+from ESSPFormer import ESSPFormerModel
+import numpy as np
+import time
+import os
+
+parser = argparse.ArgumentParser("HSI")
+parser.add_argument('--gpu_id', default='0')
+parser.add_argument('--dataset', choices=['IN', 'PU', 'SV', 'HC'], default='IN')
+parser.add_argument('--VALIDATION_SPLIT', choices=[0.80, 0.85, 0.90, 0.92, 0.95, 0.98, 0.99, 0.995, 0.999])
+
+parser.add_argument('--seed', type=int, default=0)
+parser.add_argument('--learning_rate', type=float, default=5.5e-4)
+parser.add_argument('--gamma', type=float, default=0.9)
+parser.add_argument('--weight_decay', type=float, default=0)
+parser.add_argument('-ITER', type=int, default=10)
+parser.add_argument('--epoches', type=int, default=10)
+parser.add_argument('--batch_size', type=int, default=256)
+
+parser.add_argument('--patches', type=int, default=7, help='number of patches')
+parser.add_argument('--cnn_patches', type=int, default=9, help='cnn滑窗边长')
+parser.add_argument('--n_corr', type=int, default=12, help='取相关性前n_corr')
+parser.add_argument('--n_select', type=int, default=4, help='取相关性前n_corr中的n_select个')
+parser.add_argument('--encoder_num', choices=[1, 2, 3, 4], default=3, help='encoder的数量')
+parser.add_argument('--n_pool', choices=[1, 2, 4, 5, 8], default=4, help='最后池化的步长')
+args = parser.parse_args()
+device = torch.device("cuda", 0)
+os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu_id)
+
+args.dataset = 'IN'
+args.VALIDATION_SPLIT = 0.99
+args.ITER = 3  # 10
+oabi = 4 + (args.patches - args.cnn_patches)//2
+
+cudnn.deterministic = True
+cudnn.benchmark = False
+
+multi_conf_matrix = []
+multi_KAPPA = []
+multi_OA = []
+multi_AA = []
+multi_TRAINING_TIME = []
+multi_TESTING_TIME = []
+multi_PARAMETER = []
+Functions.print_args(vars(args))
+# prepare data
+for index_iter in range(args.ITER):
+    seed = args.seed + index_iter
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # -------------------------------------------------------------------------------
+
+    data_hsi, gt_hsi, num_classes, total_pos_train, total_pos_test, total_pos_true, number_train, number_test,\
+        number_true = Functions.load_dataset(args.dataset, args.VALIDATION_SPLIT,)
+    height, width, band = data_hsi.shape
+
+    color_matrix = Functions.list_to_colormap(num_classes)
+    mirror_image, zero_image = Functions.mirror_hsi(args.dataset,height, width, band, data_hsi, patch=args.patches,
+                                                    cnn_patch=args.cnn_patches)
+
+    file_path = f'./saved_corr_pos/{args.dataset}true_patch{args.patches}_ns{args.n_select}.npy'
+    if os.path.exists(file_path):
+        print("read")
+        total_corr_pos_true = np.load(file_path)[:, -args.n_select - 1:, :]
+        total_corr_pos_test = Functions.read_corr(total_corr_pos_true, total_pos_test, args.patches, args.n_select + 1)
+        total_corr_pos_train = Functions.corr_data(zero_image, total_pos_train, args.patches, args.n_corr)
+        print(f"total_corr_pos_train  shape: {total_corr_pos_train.shape}")
+    else:
+        print("calculate")
+        total_corr_pos_true = Functions.corr_data(zero_image, total_pos_true, args.patches, args.n_select + 1)
+        total_corr_pos_test = Functions.corr_data(zero_image, total_pos_test, args.patches, args.n_select + 1)
+        total_corr_pos_train = Functions.corr_data(zero_image, total_pos_train, args.patches, args.n_corr)
+        print(f"total_corr_pos_train  shape: {total_corr_pos_train.shape}")
+        np.save(file_path, total_corr_pos_true)
+
+    corr_x_train_band, corr_x_test_band, corr_x_true, nnn_ex = Functions.corr_train_and_test_data(mirror_image, band,
+                                                                                                  total_corr_pos_train,
+                                                                                                  total_corr_pos_test,
+                                                                                                  total_corr_pos_true,
+                                                                                                  oabi,
+                                                                                                  patch=
+                                                                                                  args.cnn_patches,
+                                                                                                  nnc=args.n_corr,
+                                                                                                  nns=args.n_select)
+
+    corr_y_train, y_train, y_test, y_true = Functions.train_and_test_label(number_train, number_test, number_true,
+                                                                           num_classes, nnn_ex)
+
+    # -------------------------------------------------------------------------------
+    # load data
+
+    x_train = torch.from_numpy(corr_x_train_band.transpose(0, 1, 2)).float()
+    corr_y_train = torch.from_numpy(corr_y_train)
+    Label_train = Data.TensorDataset(x_train, corr_y_train)
+    corr_x_test = torch.from_numpy(corr_x_test_band.transpose(0, 1, 2)).float()
+    y_test = torch.from_numpy(y_test)
+    Label_test = Data.TensorDataset(corr_x_test, y_test)
+    x_true = torch.from_numpy(corr_x_true.transpose(0, 1, 2)).float()
+    y_true = torch.from_numpy(y_true)
+    Label_true = Data.TensorDataset(x_true, y_true)
+
+    label_train_loader = Data.DataLoader(Label_train, batch_size=args.batch_size, shuffle=True)
+    label_test_loader = Data.DataLoader(Label_test, batch_size=args.batch_size, shuffle=True)
+    label_true_loader = Data.DataLoader(Label_true, batch_size=args.batch_size, shuffle=False)
+
+    # -------------------------------------------------------------------------------
+    # create model
+    model = ESSPFormerModel(input_dim=band, nnss=args.n_select, num_classes=num_classes, n_pool=args.n_pool,
+                             encoder_n=args.encoder_num)
+    model = model.cuda()
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.epoches // 10, gamma=args.gamma)
+    # -------------------------------------------------------------------------------
+
+    # train
+    print("start training")
+    tic = time.time()
+    for epoch in range(args.epoches):
+        scheduler.step()
+        # train model
+        device = torch.device("cuda", 0)
+        model = model.to(device)
+        model.train()
+        train_acc, train_obj, tar_t, pre_t = Functions.train_epoch(model, label_train_loader, criterion, optimizer)
+        OA1, AA_mean1, Kappa1, AA1, confusionMatrix = Functions.output_metric(tar_t, pre_t)
+        print("Epoch: {:03d} train_loss: {:.4f} train_acc: {:.4f}"
+              .format(epoch + 1, train_obj, train_acc))
+        if (train_obj < 0.01) & (train_acc > 99.5):  # 0.1,0.05,0.01
+            break
+
+    toc = time.time()
+    multi_TRAINING_TIME.append(toc - tic)
+    print("Running Time: {:.2f}".format(toc - tic))
+    multi_PARAMETER.append(sum(p.numel() for p in model.parameters()))
+    print("**************************************************")
+    print("-------- MP Training Finished-----------")
+    pt_path = f'pt/{args.VALIDATION_SPLIT}sample/{args.dataset}_iter{index_iter}.pt'
+    path = f'pt/{args.VALIDATION_SPLIT}sample/{args.dataset}_iter{index_iter}.pt'
+    map_path = f'classification_maps/{args.VALIDATION_SPLIT}sample/{args.dataset}_iter{index_iter}'
+    torch.save(model.state_dict(), pt_path)
+
+    # test
+    model.load_state_dict(torch.load(path))
+    model.eval()
+    tic = time.time()
+
+    valid_acc, valid_obj, tar_v, pre_v = Functions.valid_epoch(model, label_test_loader, criterion, optimizer)
+    OA2, AA_mean2, Kappa2, AA2, confusionMatrix2 = Functions.output_metric(tar_v, pre_v)
+    toc = time.time()
+    multi_TESTING_TIME.append(toc - tic)
+
+    pre_u = Functions.test_epoch(model, label_true_loader, criterion, optimizer)
+    prediction_matrix = np.zeros((height, width), dtype=float)
+    for i in range(total_pos_true.shape[0]):
+        prediction_matrix[total_pos_true[i, 0], total_pos_true[i, 1]] = pre_u[i] + 1
+
+    savemat(map_path + '_matrix.mat', {'P': prediction_matrix, 'label': gt_hsi})
+    Functions.classification_map(prediction_matrix, height, width, 1600, path+'.png')
+    print('------Get classification maps successful-------')
+    print(f"result experiment No.{index_iter + 1} :")
+    print("OA: {:.4f} | AA_mean: {:.4f} | Kappa: {:.4f}".format(OA2, AA_mean2, Kappa2))
+    print("AA: ")
+    print(AA2)
+    print("**************************************************")
+    print("Parameter:")
+    multi_OA.append(OA2)
+    multi_AA.append(AA2)
+    multi_KAPPA.append(Kappa2)
+    multi_conf_matrix.append(confusionMatrix)
+
+print("---------------------------------------------")
+print("---------------------------------------------")
+print("---------------Process finished---------------------")
+
+record_path = f"records/a{args.VALIDATION_SPLIT}test_patches{args.patches}_" \
+              f"cnnpatches{args.cnn_patches}_encoder{args.encoder_num}_ncor{args.n_corr}_ns{args.n_select}" \
+              f"_npool{args.n_pool}_seed{args.seed}{args.dataset}.txt"
+
+Functions.record_output(str(vars(args)), multi_OA, multi_AA, multi_KAPPA, multi_conf_matrix,
+                        multi_TRAINING_TIME, multi_TESTING_TIME, record_path)
+AC_list = np.array(multi_AA)
+for ii in range(num_classes):
+    print("{:.4f} ± {:.4f}".format(np.mean(AC_list[:, ii]), np.std(AC_list[:, ii])))
+print()
+print("{:.4f} ± {:.4f}".format(np.mean(multi_OA), np.std(multi_OA)))
+print("{:.4f} ± {:.4f}".format(np.mean(multi_AA), np.std(multi_AA)))
+print("{:.4f} ± {:.4f}".format(np.mean(multi_KAPPA), np.std(multi_KAPPA)))
+print()
+print(f"{np.mean(multi_TRAINING_TIME):.2f}/{np.mean(multi_TESTING_TIME):.2f}")
+print(f"{np.mean(multi_PARAMETER):.0f}")
+print("-----------------All Over---------------------")
